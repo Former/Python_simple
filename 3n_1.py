ff99d2f6eff45a4e2c50a469e5b241a910c31bcc
Alexei Bezborodov <AlexeiBv@yandex.ru>
2021-10-09 21:24:52 +0300

    3n+1

---
+++ 3n_1.py
@@ -0,0 +1,20 @@
+maxx = 0
+b = 0
+for i in range(1, 1001):
+    count = 0
+    s = i # Начальное значение
+    
+    while s != 1:
+        if count >= 1000000:
+            break
+        if s % 2 == 0:
+            s = s // 2
+        else:
+            s = s * 3 + 1
+    
+        count = count + 1
+    if count >= maxx:
+        maxx = count
+        b = i
+        
+print("Для", b, "будет максимум чисел в последовательности", maxx)
