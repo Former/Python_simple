ef3815066707f8ae85599a324a150d029261624a
Alexei Bezborodov <AlexeiBv@yandex.ru>
2021-11-06 18:11:37 +0300

    Chess HTML

--- Chess_gen_HTML.py
+++ Chess_gen_HTML.py
@@ -12,7 +12,7 @@ for i_row in range(8):
     print("<tr>", end="")
     for i_col in range(8):
         color = ""
-        if (i_row + i_col)%2 == 0:
+        if (i_row + i_col + 1)%2 == 0:
             color = " bgcolor=#707070"
         print("<td"+ color + ">" + data[i_row * 8 + i_col] + "</td>", end="")
     print("</tr>")
