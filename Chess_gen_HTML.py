f22aea411221956a04c3033ea4f4434806ea22f1
Alexei Bezborodov <AlexeiBv@yandex.ru>
2021-11-06 18:09:56 +0300

    Chess HTML

---
+++ Chess_gen_HTML.py
@@ -0,0 +1,19 @@
+data =  "♜♞♝♛♚♝♞♜"\
+        "♟♟♟♟♟♟♟♟"\
+        "________"\
+        "________"\
+        "________"\
+        "________"\
+        "♙♙♙♙♙♙♙♙"\
+        "♖♘♗♕♔♗♘♖"
+
+print("<table border=1>")
+for i_row in range(8):
+    print("<tr>", end="")
+    for i_col in range(8):
+        color = ""
+        if (i_row + i_col)%2 == 0:
+            color = " bgcolor=#707070"
+        print("<td"+ color + ">" + data[i_row * 8 + i_col] + "</td>", end="")
+    print("</tr>")
+print("</table>")
