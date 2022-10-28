cb0e5e79e545dd6d4157854e3acde4f8c09ea1f5
Alexei Bezborodov <alexeibv+git3@narod.ru>
2022-10-28 11:21:27 +0300

    print source

---
+++ print_source.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 by Alexei Bezborodov <AlexeiBv@narod.ru>
+
+# Программа печатающая свой исходный код
+k="'";kk='"';s="print('k='+kk+k+kk+';kk='+k+kk+k+';s='+kk+s+kk+';'+s)";print('k='+kk+k+kk+';kk='+k+kk+k+';s='+kk+s+kk+';'+s)
