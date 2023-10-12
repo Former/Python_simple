a503a2a93a81999badbb2e675ce1ed01907c2053
Alexei Bezborodov <alexeibv+git3@narod.ru>
2023-10-12 21:17:45 +0300

    Подсказка для языка питон

--- 3n_1.py
+++ 3n_1.py
@@ -5,6 +5,8 @@ for i in range(1, 1001):
     count = 0
     s = i # Начальное значение
     
+    print('------------------')
+    
     while s != 1:
         if count >= 1000000:
             break
@@ -12,6 +14,7 @@ for i in range(1, 1001):
             s = s // 2
         else:
             s = s * 3 + 1
+        print(s, bin(s))
     
         count = count + 1
     if count >= maxx:
